d0fbd740c05847dc13a5de27bd53b43d8bfa23c1
DomenicoMartynski <domisku123@gmail.com>
2023-10-12 15:54:51 +0200

    Progress after second laboratory classes. Added html and some css

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html> 
+<head>
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+ <title>Domenico Martynski's Site</title>
+ <body>
+ <h1>My Example</h1>
+ <p>Here is some more HTML</p>
+ <div><button onclick="changeColor('red', 'powderblue')">Click me!</button></table></div>
+ <script src="js/myScript.js"></script>
+ </script>
+ </body>
+ </html>    
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+//your PHP code goes here
+include("includes/header.php");
 
 echo "Hello bruh";
+?>
