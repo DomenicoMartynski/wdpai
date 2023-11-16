5addce9fee9d3ce04b7f52197330170bfce7a20e
DomenicoMartynski <domisku123@gmail.com>
2023-11-16 15:20:30 +0100

    After 16.11.2023 classes - routing php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html> 
 <head>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
  <title>Domenico Martynski's Site</title>
@@ -9,7 +9,7 @@
  <h1>My Example</h1>
  <p>Here is some more HTML</p>
  <div><button onclick="changeColor('red', 'powderblue')">Click me!</button></table></div>
- <script src="js/myScript.js"></script>
+ <script src="public/js/myScript.js"></script>
  </script>
  </body>
  </html>    
--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 //your PHP code goes here
+require 'Routing.php';
 include("includes/header.php");
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('projects', 'DefaultController');
+Routing::run($path);
 
 echo "Hello bruh";
 ?>
